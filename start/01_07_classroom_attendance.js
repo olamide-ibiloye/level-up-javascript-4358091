8ef1e473aac7d08c6430633290ee044f2a3d4a28
olamide-ibiloye <117900493+olamide-ibiloye@users.noreply.github.com>
2023-08-02 12:27:32 +0000

    6, 7 & 8

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,24 @@
 // Write your code here
+function getStudent(classroom) {
+  const { hasTeachingAssistant, classList } = classroom;
+  let teacher, teachingAssistant, students;
+
+  if (classList.length === 0) {
+    return null;
+  }
+
+  if (hasTeachingAssistant) {
+    [teacher, teachingAssistant, ...students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+
+  return students;
+}
+
+const oneClassRoom = {
+  hasTeachingAssistant: false,
+  classList: ["Rashid", "John", "Roman", "Lisa", "Omair", "Luke"],
+};
+
+console.log(getStudent(oneClassRoom));
